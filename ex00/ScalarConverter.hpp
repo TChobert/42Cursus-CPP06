0e8edb886a723925f160af8b9c4aa11d2f4e0e08
Thomas Chobert <tchobert@f4r1s10.clusters.42paris.fr>
2025-09-10 17:27:01 +0200

    ex00 Makefile

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+
+class ScalarConverter {
+
+	private:
+
+	public:
+
+	ScalarConverter(void);
+	~ScalarConverter(void);
+
+	static void convert(std::string& literal);
+};
