a6628453d3de1d7192ae818d23d544ebaae0259b
Thomas Chobert <tchobert@f4r1s10.clusters.42paris.fr>
2025-09-10 17:54:21 +0200

    structure of class ScalarConverter

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -3,3 +3,45 @@
 ScalarConverter::ScalarConverter(void) {}
 
 ScalarConverter::~ScalarConverter(void) {}
+
+literalType getLiteralType(std::string& literal) {
+
+	if (literal.size() == 1 && (!std::isdigit(static_cast<unsigned char>(literal[0]))))
+		return (CHAR);
+}
+
+void convertFloat(std::string& literal) {
+
+}
+
+void convertInt(std::string& literal) {
+
+}
+
+void convertDouble(std::string& literal) {
+
+}
+
+void convertChar(std::string& literal) {
+
+}
+
+void ScalarConverter::convert(std::string& literal) {
+
+	if (literal.empty()) {
+		std::cerr << "Error: literal can't be empty." << std::endl;
+	}
+
+	literalType type = getLiteralType(literal);
+
+	switch (type) {
+		case CHAR:
+			return (convertChar(literal));
+		case INT:
+			return (convertInt(literal));
+		case FLOAT:
+			return (convertFloat(literal));
+		case DOUBLE:
+			return (convertDouble(literal));
+	}
+}
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,16 +1,28 @@
 #pragma once
 
+#include <string>
 #include <iostream>
+#include <limits>
 #include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <climits>
+
+enum literalType {
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE
+};
 
 class ScalarConverter {
 
 	private:
 
-	public:
-
 	ScalarConverter(void);
 	~ScalarConverter(void);
+
+	public:
 
 	static void convert(std::string& literal);
 };
